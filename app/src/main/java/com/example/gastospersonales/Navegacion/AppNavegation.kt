33dba9ee606772a09dae9193ddd0525df94a5afc
Checky23 <146308146+Checky23@users.noreply.github.com>
2026-09-13 20:50:28 -0600

    AppNavegacion actualizado con el push de Marcos y Mio Aura

--- app/src/main/java/com/example/gastospersonales/Navegacion/AppNavegation.kt
+++ app/src/main/java/com/example/gastospersonales/Navegacion/AppNavegation.kt
@@ -4,45 +4,61 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.gastospersonales.FireBase.VerificacionDeUsuario
 import com.example.gastospersonales.Pantallas.InicioDeSesionScreen
+import com.example.gastospersonales.Pantallas.InicioScreen
 import com.example.gastospersonales.Pantallas.RegistroScreen
-import com.example.gastospersonales.Pantallas.SplashScreenConstraint
 
 @Composable
 fun AppNavigation() {
 
-    // Control de la navegación entre pantalla
+    // Controla la navegación entre pantallas
     val navController = rememberNavController()
+    val usuarioLogeado = VerificacionDeUsuario()
 
-    NavHost(
-        navController = navController,
-        // arranca en el splash, no en el login
-        startDestination = "splash"
-    ) {
+    if ( usuarioLogeado == true){
+        NavHost(
+            navController = navController,
+            // arranca en el splash, no en el login
 
-        // RUTA DEL SPLASH SCREEN
-        composable("splash") {
-            SplashScreenConstraint(
-                onAppReady = { destino ->
-                    // Recibo login
-                    if (destino == "login") {
-                        navController.navigate(Screen.InicioDeSesionScreen.ruta) {
-                            // Destruyo el splash para que el botón de "Atrás" del celular no vuelva a él
-                            popUpTo("splash") { inclusive = true }
+            startDestination = "splash"
+        ) {
+
+            // RUTA DEL SPLASH SCREEN
+            composable("splash") {
+                SplashScreenConstraint(
+                    onAppReady = { destino ->
+                        // Recibo login
+                        if (destino == "login") {
+                            navController.navigate(Screen.InicioDeSesionScreen.ruta) {
+                                // Destruyo el splash para que el botón de "Atrás" del celular no vuelva a él
+                                popUpTo("splash") { inclusive = true }
+                            }
                         }
                     }
-                }
-            )
-        }
+                )
+            }
 
-        // Pantalla 1
-        composable(route = Screen.InicioDeSesionScreen.ruta) {
-            InicioDeSesionScreen()
-        }
+            //  Inicio - Temporal
+            composable(Screen.InicioScreen.ruta) {
+                InicioScreen(navController)
+            }
 
-        // Pantalla 2
-        composable(route = Screen.RegistroScreen.ruta) {
-            RegistroScreen()
+            // RegistroDeSesion
+            composable(Screen.RegistroScreen.ruta) {
+                RegistroScreen()
+            }
+        }
+    } else{
+        NavHost(
+            navController = navController,
+            startDestination = Screen.InicioDeSesionScreen.ruta
+        ){
+            //  InicioDeSesion
+            composable(Screen.InicioDeSesionScreen.ruta){
+                InicioDeSesionScreen(navController)
+            }
         }
     }
+
 }
